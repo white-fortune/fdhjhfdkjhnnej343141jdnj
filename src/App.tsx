9033d2cc5e337314a94361a79bad3896fcd3bd87
white-fortune <whitefortune271828@gmail.com>
2025-11-14 01:57:35 +0600

    new post-container

--- src/App.tsx
+++ src/App.tsx
@@ -394,56 +394,56 @@ function Divider({ extendedClass }: { extendedClass?: string }) {
 
 function PostContainer() {
 	return (
-		<div className="post-container 
-			min-h-[50vh] max-h-[70vh] w-[90vh] m-10 
+		<div className="post-container
+			w-[90vh] m-10 h-[50vh]
 			flex flex-col gap-1
-			overflow-hidden
 			font-['Space_Grotesk'] bg-[var(--leftpanel-maincontainer-clr)] border rounded-[20px] 
 		">
-			<div className="topbar
-				w-full 
-				min-h-[10vh] 
+			<div className="content
+				w-full h-[80%]
 				flex flex-row
 			">
-				<div className="post-metadata w-[90%] flex">
-					<div className="author-profile-pic w-[15%] min-[2000px]:w-[10%] flex justify-center items-center">
-						<svg className='h-[60px] w-[60px] min-[2000px]:h-[65px] min-[2000px]:w-[65px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill='currentColor'>
+				<div className="left-side w-[15%]">
+					<div className="author-profile-pic h-[25%] flex justify-center items-center">
+						<svg className='h-[60px] w-[60px] min-[2000px]:h-[70px] min-[2000px]:w-[70px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill='currentColor'>
 							<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/>
 						</svg>
 					</div>
-					<div className="post-header w-[85%] flex flex-col place-self-center gap-1">
-						<div className="author-fullname w-full h-[30%]">
-							<span className='text-[12px] min-[2000px]:text-base'>Anirban Biswas</span>
+				</div>
+				<div className="metadata w-[75%] flex flex-col">
+					<div className="author-section h-[18%] flex items-center">
+						<div className="author-fullname">
+							<span>Anirban Biswas</span>
 						</div>
-						<div className="post-title-input w-full h-[70%]">
+					</div>
+					<div className="post-data h-[82%] flex flex-col">
+						<div className="post-title-input w-full h-[20%]">
 							<input type="text" className='w-full h-full outline-none bg-transparent text-[rgba(0,0,0,0.7)] text-[18px] min-[2000px]:text-[20px]' placeholder='Title *' />
+						</div>
+						<div className="post-desc-input scrollbar
+							h-[80%] w-full
+							flex self-center
+							overflow-y-auto
+						">
+							<textarea
+								className="w-full resize-none outline-none bg-transparent min-[2000px]:text-[18px]"
+								placeholder="What are you working on, Anirban Biswas?"
+							/>
 						</div>
 					</div>
 				</div>
-				<div className="close-button w-[10%] flex justify-center items-center">
-					<svg width="24" height="24" viewBox="0 0 24 24" className='fill-[#D9D9D9] hover:fill-gray-400 cursor-pointer' xmlns="http://www.w3.org/2000/svg">
-						<rect width="24" height="24" rx="12"/>
-						<path clip-rule="evenodd" d="M16.8104 8.10388C16.8705 8.04391 16.9181 7.9727 16.9507 7.89431C16.9832 7.81593 16.9999 7.7319 17 7.64704C17.0001 7.56217 16.9834 7.47813 16.951 7.3997C16.9185 7.32127 16.871 7.25 16.811 7.18996C16.751 7.12991 16.6798 7.08227 16.6014 7.04974C16.5231 7.01722 16.439 7.00045 16.3542 7.0004C16.2693 7.00035 16.1853 7.01701 16.1068 7.04944C16.0284 7.08187 15.9571 7.12942 15.8971 7.1894L11.9999 11.0866L8.10386 7.1894C7.9826 7.06813 7.81813 7 7.64663 7C7.47513 7 7.31066 7.06813 7.18939 7.1894C7.06813 7.31066 7 7.47514 7 7.64664C7 7.81814 7.06813 7.98261 7.18939 8.10388L11.0866 12L7.18939 15.8961C7.12935 15.9562 7.08172 16.0274 7.04922 16.1059C7.01673 16.1844 7 16.2684 7 16.3534C7 16.4383 7.01673 16.5224 7.04922 16.6008C7.08172 16.6793 7.12935 16.7506 7.18939 16.8106C7.31066 16.9319 7.47513 17 7.64663 17C7.73155 17 7.81563 16.9833 7.89408 16.9508C7.97254 16.9183 8.04382 16.8706 8.10386 16.8106L11.9999 12.9134L15.8971 16.8106C16.0184 16.9317 16.1828 16.9997 16.3542 16.9996C16.5256 16.9995 16.6899 16.9313 16.811 16.81C16.9321 16.6888 17.0001 16.5244 17 16.353C16.9999 16.1816 16.9317 16.0172 16.8104 15.8961L12.9133 12L16.8104 8.10388Z" fill="black" fill-opacity="0.4"/>
-					</svg>
+
+				<div className="right-side w-[10%]">
+					<div className="close-button h-[20%] flex justify-center items-center">
+						<svg width="24" height="24" viewBox="0 0 24 24" className='fill-[#D9D9D9] hover:fill-gray-400 cursor-pointer' xmlns="http://www.w3.org/2000/svg">
+							<rect width="24" height="24" rx="12"/>
+							<path clip-rule="evenodd" d="M16.8104 8.10388C16.8705 8.04391 16.9181 7.9727 16.9507 7.89431C16.9832 7.81593 16.9999 7.7319 17 7.64704C17.0001 7.56217 16.9834 7.47813 16.951 7.3997C16.9185 7.32127 16.871 7.25 16.811 7.18996C16.751 7.12991 16.6798 7.08227 16.6014 7.04974C16.5231 7.01722 16.439 7.00045 16.3542 7.0004C16.2693 7.00035 16.1853 7.01701 16.1068 7.04944C16.0284 7.08187 15.9571 7.12942 15.8971 7.1894L11.9999 11.0866L8.10386 7.1894C7.9826 7.06813 7.81813 7 7.64663 7C7.47513 7 7.31066 7.06813 7.18939 7.1894C7.06813 7.31066 7 7.47514 7 7.64664C7 7.81814 7.06813 7.98261 7.18939 8.10388L11.0866 12L7.18939 15.8961C7.12935 15.9562 7.08172 16.0274 7.04922 16.1059C7.01673 16.1844 7 16.2684 7 16.3534C7 16.4383 7.01673 16.5224 7.04922 16.6008C7.08172 16.6793 7.12935 16.7506 7.18939 16.8106C7.31066 16.9319 7.47513 17 7.64663 17C7.73155 17 7.81563 16.9833 7.89408 16.9508C7.97254 16.9183 8.04382 16.8706 8.10386 16.8106L11.9999 12.9134L15.8971 16.8106C16.0184 16.9317 16.1828 16.9997 16.3542 16.9996C16.5256 16.9995 16.6899 16.9313 16.811 16.81C16.9321 16.6888 17.0001 16.5244 17 16.353C16.9999 16.1816 16.9317 16.0172 16.8104 15.8961L12.9133 12L16.8104 8.10388Z" fill="black" fill-opacity="0.4"/>
+						</svg>
+					</div>
 				</div>
 			</div>
 
-			<div className="content scrollbar
-				min-h-[30vh] w-[95%]
-				flex self-center
-				overflow-y-auto
-			">
-				<textarea
-					className="w-full resize-none outline-none p-3 bg-transparent min-[2000px]:text-[18px]"
-					placeholder="What are you working on, Anirban Biswas?"
-					onInput={(e) => {
-						e.target.style.height = "auto";
-						e.target.style.height = e.target.scrollHeight + "px";
-					}}
-				/>
-			</div>
-
-			<PostSectionTools extendedClass='p-5 w-full flex justify-between flex-grow' inAction={true} />
+			<PostSectionTools extendedClass='p-5 w-full flex justify-between h-[20%]' inAction={true} />
 		</div>
 	)
 }
