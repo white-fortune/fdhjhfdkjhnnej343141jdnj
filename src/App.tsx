de344563f90c9428256474327e720e7d269c4a45
white-fortune <whitefortune271828@gmail.com>
2025-11-13 22:03:07 +0600

    text-only post-container for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -165,9 +165,9 @@ function NavigationPanel({ sidebar, hamBurgerMenuRef }: { sidebar: StateControll
 	)
 }
 
-function PostSectionTool({ label, children }: { label: string, children: React.ReactElement<any> }) {
+function PostSectionTool({ label, children, inAction }: { label: string, children: React.ReactElement<any>, inAction?: boolean }) {
 	return (
-		<div className="tool gap-[2px] w-full flex items-center p-1 cursor-pointer">
+		<div className={`tool gap-[2px] w-full flex items-center p-[5px] cursor-pointer rounded-[10px] ${inAction && 'hover:bg-gray-200'}`}>
 			{ React.cloneElement(children, {
 				className: 'h-[20px] w-[20px] min-[2000px]:h-[28px] min-[2000px]:w-[28px]'
 			}) }
@@ -202,29 +202,24 @@ function PostSectionIntro() {
 		</div>
 	)
 }
-function PostSectionTools() {
+function PostSectionTools({ extendedClass, inAction = false }: { extendedClass : string, inAction?: boolean }) {
 	return (
-		<div className="tools
-			p-2
-			w-full h-[30%]
-			hidden 
-			md:flex justify-between
-		">
+		<div className={`tools ${extendedClass}`}>
 			<div className="tools-left 
 				gap-[15px] flex items-end
 			">
-				<PostSectionTool label='Media'>
+				<PostSectionTool label='Media' inAction={inAction}>
 					<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25V19.75C2 20.99 3.01 22 4.25 22H19.75C20.99 22 22 20.99 22 19.75V4.25C22 3.01 20.99 2 19.75 2ZM4.25 3.5H19.75C20.163 3.5 20.5 3.837 20.5 4.25V13.926L16.642 10.068C16.502 9.928 16.312 9.848 16.112 9.848H16.109C15.909 9.848 15.716 9.928 15.577 10.072L11.26 14.456L9.447 12.65C9.307 12.51 9.117 12.43 8.917 12.43C8.724 12.4 8.522 12.51 8.382 12.657L3.5 17.642V4.25C3.5 3.837 3.837 3.5 4.25 3.5ZM3.506 19.78L8.924 14.246L15.206 20.5H4.25C3.848 20.5 3.523 20.178 3.506 19.78ZM19.75 20.5H17.33L12.323 15.513L16.115 11.663L20.5 16.047V19.75C20.5 20.163 20.163 20.5 19.75 20.5Z" fill="#42ACDE"/>
 						<path d="M8.86799 9.85097C9.71961 9.85097 10.41 9.16059 10.41 8.30897C10.41 7.45734 9.71961 6.76697 8.86799 6.76697C8.01637 6.76697 7.32599 7.45734 7.32599 8.30897C7.32599 9.16059 8.01637 9.85097 8.86799 9.85097Z" fill="#42ACDE"/>
 					</svg>
 				</PostSectionTool>
-				<PostSectionTool label='Poll'>
+				<PostSectionTool label='Poll' inAction={inAction}>
 					<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M20.222 9.16003H18.888C18.903 9.07003 18.916 8.97803 18.916 8.88303V6.57003C18.916 5.59003 18.119 4.79303 17.138 4.79303H3.5V3.35803C3.5 2.94403 3.164 2.60803 2.75 2.60803C2.336 2.60803 2 2.94403 2 3.35803V20.83C2 21.245 2.336 21.58 2.75 21.58C3.164 21.58 3.5 21.245 3.5 20.83V19.396H14.056C15.036 19.396 15.834 18.599 15.834 17.619V15.306C15.834 15.211 15.82 15.119 15.806 15.028H20.223C21.203 15.028 22.001 14.23 22.001 13.25V10.94C22.001 9.95703 21.204 9.16003 20.223 9.16003H20.222ZM17.14 6.29303C17.292 6.29303 17.417 6.41703 17.417 6.57003V8.88003C17.417 9.03403 17.292 9.16003 17.139 9.16003H3.5V6.29003H17.14V6.29303ZM14.333 15.307V17.619C14.333 17.772 14.208 17.896 14.055 17.896H3.5V15.028H14.056C14.209 15.028 14.333 15.154 14.333 15.308V15.307ZM20.5 13.25C20.5 13.403 20.375 13.527 20.222 13.527H3.5V10.66H20.222C20.375 10.66 20.5 10.784 20.5 10.937V13.25Z" fill="#42ACDE"/>
 					</svg>
 				</PostSectionTool>
-				<PostSectionTool label='Event'>
+				<PostSectionTool label='Event' inAction={inAction}>
 					<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<g clip-path="url(#clip0_9783_7329)">
 							<path d="M-37.9 18C-38 17.9 -38 17.9 -38 17.8C-37.9 17.8 -37.9 17.9 -37.9 18ZM18 2.19998H16.7V1.89998C16.7 1.49998 16.4 1.09998 15.9 1.09998C15.5 1.09998 15.1 1.39998 15.1 1.89998V2.19998H7.7V1.89998C7.7 1.49998 7.4 1.09998 6.9 1.09998C6.5 1.09998 6.1 1.39998 6.1 1.89998V2.19998H4.8C3.4 2.19998 2.3 3.29998 2.3 4.69998V17.8C2.3 19.2 3.4 20.3 4.8 20.3H7.7C8.1 20.3 8.5 20 8.5 19.5C8.5 19.1 8.2 18.7 7.7 18.7H4.8C4.2 18.7 3.8 18.2 3.8 17.7V7.89998C3.8 7.59998 4.2 7.19998 4.8 7.19998H18C18.6 7.19998 19 7.59998 19 7.89998V9.69998C19 10.1 19.3 10.5 19.8 10.5C20.2 10.5 20.6 10.2 20.6 9.69998V4.69998C20.5 3.29998 19.4 2.19998 18 2.19998ZM19 5.89998C18.7 5.79998 18.3 5.69998 18 5.69998H4.8C4.4 5.69998 4.1 5.79998 3.8 5.89998V4.69998C3.8 4.09998 4.3 3.69998 4.8 3.69998H6.1V4.19998C6.1 4.59998 6.4 4.99998 6.9 4.99998C7.3 4.99998 7.7 4.69998 7.7 4.19998V3.69998H15.2V4.19998C15.2 4.59998 15.5 4.99998 16 4.99998C16.4 4.99998 16.8 4.69998 16.8 4.19998V3.69998H18C18.6 3.69998 19 4.19998 19 4.69998V5.89998Z" fill="#42ACDE"/>
@@ -263,7 +258,12 @@ function PostSection() {
 					
 				<PostSectionIntro />
 
-				<PostSectionTools />
+				<PostSectionTools extendedClass="
+					p-2
+					w-full h-[30%]
+					hidden 
+					md:flex justify-between
+				"/>
 			</div>
 			<Divider extendedClass='my-7' />
 		</div>
@@ -389,6 +389,62 @@ function FeedSection() {
 function Divider({ extendedClass }: { extendedClass?: string }) {
 	return (
 		<div className={`w-[90%] h-[1px] bg-gray-300 md:w-full ${extendedClass}`}></div>
+	)
+}
+
+function PostContainer() {
+	return (
+		<div className="post-container 
+			min-h-[50vh] max-h-[70vh] w-[90vh] m-10 
+			flex flex-col gap-1
+			overflow-hidden
+			font-['Space_Grotesk'] bg-[var(--leftpanel-maincontainer-clr)] border rounded-[20px] 
+		">
+			<div className="topbar
+				w-full 
+				min-h-[10vh] 
+				flex flex-row
+			">
+				<div className="post-metadata w-[90%] flex">
+					<div className="author-profile-pic w-[15%] min-[2000px]:w-[10%] flex justify-center items-center">
+						<svg className='h-[60px] w-[60px] min-[2000px]:h-[65px] min-[2000px]:w-[65px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill='currentColor'>
+							<path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/>
+						</svg>
+					</div>
+					<div className="post-header w-[85%] flex flex-col place-self-center gap-1">
+						<div className="author-fullname w-full h-[30%]">
+							<span className='text-[12px] min-[2000px]:text-base'>Anirban Biswas</span>
+						</div>
+						<div className="post-title-input w-full h-[70%]">
+							<input type="text" className='w-full h-full outline-none bg-transparent text-[rgba(0,0,0,0.7)] text-[18px] min-[2000px]:text-[20px]' placeholder='Title *' />
+						</div>
+					</div>
+				</div>
+				<div className="close-button w-[10%] flex justify-center items-center">
+					<svg width="24" height="24" viewBox="0 0 24 24" className='fill-[#D9D9D9] hover:fill-gray-400 cursor-pointer' xmlns="http://www.w3.org/2000/svg">
+						<rect width="24" height="24" rx="12"/>
+						<path clip-rule="evenodd" d="M16.8104 8.10388C16.8705 8.04391 16.9181 7.9727 16.9507 7.89431C16.9832 7.81593 16.9999 7.7319 17 7.64704C17.0001 7.56217 16.9834 7.47813 16.951 7.3997C16.9185 7.32127 16.871 7.25 16.811 7.18996C16.751 7.12991 16.6798 7.08227 16.6014 7.04974C16.5231 7.01722 16.439 7.00045 16.3542 7.0004C16.2693 7.00035 16.1853 7.01701 16.1068 7.04944C16.0284 7.08187 15.9571 7.12942 15.8971 7.1894L11.9999 11.0866L8.10386 7.1894C7.9826 7.06813 7.81813 7 7.64663 7C7.47513 7 7.31066 7.06813 7.18939 7.1894C7.06813 7.31066 7 7.47514 7 7.64664C7 7.81814 7.06813 7.98261 7.18939 8.10388L11.0866 12L7.18939 15.8961C7.12935 15.9562 7.08172 16.0274 7.04922 16.1059C7.01673 16.1844 7 16.2684 7 16.3534C7 16.4383 7.01673 16.5224 7.04922 16.6008C7.08172 16.6793 7.12935 16.7506 7.18939 16.8106C7.31066 16.9319 7.47513 17 7.64663 17C7.73155 17 7.81563 16.9833 7.89408 16.9508C7.97254 16.9183 8.04382 16.8706 8.10386 16.8106L11.9999 12.9134L15.8971 16.8106C16.0184 16.9317 16.1828 16.9997 16.3542 16.9996C16.5256 16.9995 16.6899 16.9313 16.811 16.81C16.9321 16.6888 17.0001 16.5244 17 16.353C16.9999 16.1816 16.9317 16.0172 16.8104 15.8961L12.9133 12L16.8104 8.10388Z" fill="black" fill-opacity="0.4"/>
+					</svg>
+				</div>
+			</div>
+
+			<div className="content scrollbar
+				min-h-[30vh] w-[95%]
+				flex self-center
+				overflow-y-auto
+			">
+				<textarea
+					className="w-full resize-none outline-none p-3 bg-transparent min-[2000px]:text-[18px]"
+					placeholder="What are you working on, Anirban Biswas?"
+					onInput={(e) => {
+						e.target.style.height = "auto";
+						e.target.style.height = e.target.scrollHeight + "px";
+					}}
+				/>
+			</div>
+
+			<PostSectionTools extendedClass='p-5 w-full flex justify-between flex-grow' inAction={true} />
+		</div>
 	)
 }
 
